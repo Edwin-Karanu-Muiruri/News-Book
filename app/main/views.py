4ed8fcf581209e130351e2ca2adb81fbfabb0dba
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-04-26 23:34:20 +0300

    Created an index.html file in the templates and added a heading.

---
+++ app/main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    '''
+    View root page function. It returns the index page as its data
+    '''
+    return render_template('index.html')
