35a6b27680f6bab144afe6065b24f261f7f323f3
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-04-27 06:48:46 +0300

    Created the index function in the views.

--- app/main/views.py
+++ app/main/views.py
@@ -7,4 +7,15 @@ def index():
     '''
     View root page function. It returns the index page as its data
     '''
-    
+    general = get_articles('top-headlines','category=general',4)
+    science = get_articles('top-headlines','category=science',4)
+    technology = get_articles('top-headlines','category=technology',4)
+    health = get_articles('top-headlines','category=health',4)
+    entertainment = get_articles('top-headlines','category=entertainment',4)
+    business = get_articles('top-headlines','category=business',4)
+
+    query = request.args.get('query')
+    if query:
+        return redirect(url_for('main.search',keywords = query))
+    else:
+        return render_template('index.html', title = 'Home | General', general = general, science = science, technology = technology, health = health,entertainment = entertainment, business = business)
